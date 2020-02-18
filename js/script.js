23a523716dc86b0afc3888fcf37ca4417d675f61
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-02-18 18:33:26 +0100

    bugfix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Sistema di calcolo per costo biglietto treno
 // Informazioni: età |  Km da percorrere - costo per Km 0.21 €
-// Sconti applicati: ≤ 18 anni - 20% | > 65 - 40%
+// Sconti applicati: < 18 anni - 20% | >= 65 - 40%
 
 
 // dati in ingresso
@@ -10,15 +10,15 @@ var priceForKm = 0.21; // costo per Km 0.21 €
 var ticketPrice = (priceForKm * distance); // costo del biglietto al netto degli sconti
 //console.log(ticketPrice); //debug
 
-var discountYoung = ticketPrice * 0.20; // sconto ≤ 18 anni - 20%
-var discountOld = ticketPrice * 0.40; // sconto > 65 - 40%
+var discountYoung = ticketPrice * 0.20; // sconto < 18 anni - 20%
+var discountOld = ticketPrice * 0.40; // sconto >= 65 - 40%
 
 var ticketType = "Standard";
 
 if (age >= 65) {
     var ticketFinalPrice = ticketPrice - discountOld;
     ticketType = "Senior";
-} else if (age <= 18) {
+} else if (age < 18) {
     var ticketFinalPrice = ticketPrice - discountYoung;
     ticketType = "Junior";
 } else {
